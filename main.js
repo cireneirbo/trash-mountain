0937cff536f3be98919fef058f7bdf108430d2ab
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-06-10 18:42:47 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,3 @@
+const opener = require('opener');
+const destinationURL = "https://www.google.com/maps/place/34%C2%B057'30.5%22N+108%C2%B013'55.3%22W/@34.9581745,-108.2322987,202m/data=!3m1!1e3!4m5!3m4!1s0x0:0x0!8m2!3d34.9584759!4d-108.2320175";
+opener(destinationURL);
